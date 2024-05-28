ff7dada7983ce86460731d7dbbef024c7eae8c31
Jaikishan Gupta <jaikishangupta754@gmail.comm>
2024-05-29 00:31:30 +0530

    customreactCreating

---
+++ customReact/customreact.js
@@ -0,0 +1,14 @@
+function customRender(reactElement, mainContainer) {}
+
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit google",
+};
+
+const mainContainer = document.querySelector("#root");
+
+customRender(reactElement, mainContainer);
